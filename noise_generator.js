e79d16065b52fb6bc16443641bd62b4036b7b231
Brennan Wilkes <brennan@codexwilkes.com>
2020-05-04 11:24:27 -0700

    Island generator

--- noise_generator.js
+++ noise_generator.js
@@ -1,38 +1,81 @@
+
+//nomralize a value to between 0 and 1 based on min and max
 function normalize(val, min, max){
 	return (val-min)/(max-min);
 }
 
-function gen_noise_map(width, height, scale, oct, persist, lac){
+//nomralize a map to between 0 and 1 based on min and max
+function normalize_map(map){
+	for(let x=0;x<map.length;x++){
+		for(let y=0;y<map[x].length;y++){
+			map[x][y] = normalize(map[x][y],map.minHeight, map.maxHeight);
+		}
+	}
+	return map;
+}
+
+function gen_noise_map(width, height, scale, oct, persist, lac, seed=Math.random()*1000){
+
+	noise.seed(seed);
 
 	let xx,yy;
-
-	let maxHeight = Number.NEGATIVE_INFINITY;
-	let minHeight = Number.POSITIVE_INFINITY;
-
 	let map = new Array(width);
+
+	map.maxHeight = Number.NEGATIVE_INFINITY;
+	map.minHeight = Number.POSITIVE_INFINITY;
+
 	for(let x=0;x<width;x++){
 		map[x] = new Array(height);
 		for(let y=0;y<height;y++){
+
+			//initialize
 			map[x][y] = 0;
 			let amp = 1;
 			let freq = 1;
 
+			//iterate over octaves
 			for(let i=0; i < oct; i++){
+
+				//adjust coordinates
 				xx = x / scale * freq
 				yy = y / scale * freq;
+
+				//generate base noise
 				map[x][y] += (noise.perlin2(xx, yy)*2-1) * amp;
+
+				//update modifiers
 				amp *= persist;
 				freq *= lac;
 			}
 
-			maxHeight = Math.max(maxHeight, map[x][y]);
-			minHeight = Math.min(minHeight, map[x][y]);
+			//update minimax
+			map.maxHeight = Math.max(map.maxHeight, map[x][y]);
+			map.minHeight = Math.min(map.minHeight, map[x][y]);
 		}
 	}
+
+	//normalize
+	return normalize_map(map);
+}
+
+
+function dist(x,y,w,h){
+	return Math.max(normalize(0.5-Math.sqrt(Math.pow(Math.abs(0.5-(x/w)),2)+Math.pow(Math.abs(0.5-(y/h)),2)),0,0.5),0);
+}
+
+//25,8,8,0.75
+function gen_island(width, height){
+	let map = gen_noise_map(width, height, 25,8,8,0.75);
+	map.maxHeight = 1;
+	map.minHeight = -0.5;
+	map = normalize_map(map);
+
 	for(let x=0;x<width;x++){
 		for(let y=0;y<height;y++){
-			map[x][y] = normalize(map[x][y],minHeight, maxHeight);
+			map[x][y] *= dist(x,y,width,height);
 		}
 	}
+
+
 	return map;
 }
--- tangaroa.js
+++ tangaroa.js
@@ -1,48 +1,50 @@
-const MAX_DIM = Math.min(Math.max(document.documentElement.clientWidth, window.innerWidth || 0), Math.max(document.documentElement.clientHeight, window.innerHeight || 0));
-
-const MAP_SIZE = 25;
-
-const TILE_SIZE = Math.round(MAX_DIM*0.9/MAP_SIZE);
-const CANVAS_SIZE = TILE_SIZE*MAP_SIZE;
+const MAX_X = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const MAX_Y = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 var canvas = document.getElementById("map");
 var ctx = canvas.getContext("2d");
 
 var map;
 
-function dist(x,y){
-	return 1-Math.sqrt(Math.pow(Math.abs(0.5-(x/TILE_SIZE)),2)+Math.pow(Math.abs(0.5-(y/TILE_SIZE)),2));
-}
 
-function islandify(height,dis,scale){
-	return height*Math.pow(dis*scale,0.8);
-}
 
 function setUp(){
-	canvas.width = CANVAS_SIZE;
-	canvas.height = CANVAS_SIZE;
-	canvas.style.margin = Math.round(MAX_DIM*0.05)+"px";
-
+	canvas.width = MAX_X;
+	canvas.height = MAX_Y;
 
 	noise.seed(Math.random()*1000);
 
-	map = gen_noise_map(CANVAS_SIZE, CANVAS_SIZE, 50, 4, 0.5, 2);
-
-
+	//map = gen_noise_map(MAX_X, MAX_Y, 50, 4, 0.5, 2);
+	map = gen_island(MAX_X,MAX_Y);
 
 	drawMap();
 
 }
 function drawMap(){
-	for(let i=0;i<CANVAS_SIZE;i++){
-		for(let j=0; j<CANVAS_SIZE; j++){
-			ctx.fillStyle = "rgb("+map[i][j]*255+", "+map[i][j]*255+", "+map[i][j]*255+")";
+	for(let i=0;i<MAX_X;i++){
+		for(let j=0; j<MAX_Y; j++){
+			if(map[i][j]<0.4){
+				ctx.fillStyle = "darkblue";
+			}
+			else if(map[i][j]<0.5){
+				ctx.fillStyle = "blue";
+			}
+			else if(map[i][j] < 0.55){
+				ctx.fillStyle = "coral";
+			}
+			else{
+				ctx.fillStyle = "green";
+			}
+
 			ctx.fillRect(i,j,1,1);
 		}
 	}
 }
 
-function debug_update(scale,oct,persist,lac){
-	map = gen_noise_map(CANVAS_SIZE, CANVAS_SIZE, scale,oct,persist,lac);
+function debug_update(scale,oct,persist,lac,reseed=false){
+	if(reseed){
+		noise.seed(Math.random()*1000);
+	}
+	map = gen_noise_map(MAX_X, MAX_Y, scale,oct,persist,lac);
 	drawMap();
 }
